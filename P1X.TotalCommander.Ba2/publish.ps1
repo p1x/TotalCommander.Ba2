bfa92df1b1d246c35454637012a3ef10b58b8510
p1x <p1xel.com@gmail.com>
2023-11-09 15:46:12 +0300

    Create autoinstall zip during publish. Cleanup project file.

--- P1X.TotalCommander.Ba2/publish.ps1
+++ P1X.TotalCommander.Ba2/publish.ps1
@@ -1,4 +1,7 @@
 ﻿cd $PSScriptRoot
 dotnet publish -r win-x64 -p:NativeLib=Shared
-copy $PSScriptRoot\bin\Release\net8.0\win-x64\publish\TotalCommander.Ba2.dll $PSScriptRoot\bin\Release\net8.0\win-x64\publish\TotalCommander.Ba2.wcx64
+mkdir $PSScriptRoot\bin\Release\net8.0\win-x64\publish\TotalCommander.Ba2 -Force
+copy $PSScriptRoot\bin\Release\net8.0\win-x64\publish\TotalCommander.Ba2.dll $PSScriptRoot\bin\Release\net8.0\win-x64\publish\TotalCommander.Ba2\TotalCommander.Ba2.wcx64
+copy $PSScriptRoot\pluginst.inf $PSScriptRoot\bin\Release\net8.0\win-x64\publish\TotalCommander.Ba2\pluginst.inf
+Compress-Archive -Path $PSScriptRoot\bin\Release\net8.0\win-x64\publish\TotalCommander.Ba2\* -DestinationPath $PSScriptRoot\bin\Release\net8.0\win-x64\publish\TotalCommander.Ba2.zip -CompressionLevel Optimal -Force
 echo done!
